9c1fee4f003f978ea0e03efadb0a1ccdbbb90208
Jamie Won <jamie.won.2000@gmail.com>
2022-01-22 13:29:36 -0500

    poitns are displayed, point information needs some work

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,20 @@ import {Route, Switch} from 'react-router-dom';
 import Home from './Pages/Home';
 import PageNotFound from './Pages/PageNotFound';
 
+
+const markerColors = {
+  waste: '#ff3d00',
+  local_sorting_facility: '#ffee33',
+  local_recycling_facility: '#91ff35',
+  regional_sorting_facility: '#dd33fa',
+  regional_recycling_facility: '#33eaff',
+}
+
 function App() {
   return (
     <div className='App'>
       <Switch>
-        <Route exact path = '/' render = {props => < Home {...props}/>}/> 
+        <Route exact path = '/' render = {props => < Home markerColors={markerColors} {...props}/>}/> 
         <Route render ={() => <PageNotFound />} />
       </Switch>
       </div>
