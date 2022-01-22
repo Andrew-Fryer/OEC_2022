c1bc26e76e6ed0bd5efd3fcfd5e37ae074d93bd4
Andrew Fryer <andrew.fryer@queensu.ca>
2022-01-22 12:52:42 -0500

    list endpoint works

--- src/app.py
+++ src/app.py
@@ -1,28 +1,29 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 from parse import parse
 from alg import alg
-from plot import plot
 
 
 app = Flask(__name__)
 
 dfs = parse()
 
-# @app.route("/alg/<int:a>/<int:b>/<string:csv_file>")
+@app.route("/list_files", methods=['GET'])
+def list_files():
+    result = list(dfs.keys())
+    print(type(result), result)
+    return jsonify(result)
+
 @app.route("/alg", methods=['GET'])
-# def do_alg(a, b, csv_file):
 def do_alg():
     args = request.args.to_dict()
     a = args.get('a')
     b = args.get('b')
     csv_file = args.get('csv_file')
-    return f'a: {a}, b: {b}, csv_file: {csv_file}'
 
-    a = 0.6
-    b = 0.4
     df = dfs[csv_file]
     result = alg(df, a, b)
-    return result
+
+    return {}
 
 print('done')
--- test/test.sh
+++ test/test.sh
@@ -1,1 +1,2 @@
+curl -i "localhost:5000/list_files"
 curl -i "localhost:5000/alg?a=0.9&b=0.1&csv_file=test_10_equal.csv"
