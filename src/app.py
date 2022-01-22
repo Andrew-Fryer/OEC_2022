8d1b9d5e9679823e9c7f3d596d2a4b58bbef0c43
Andrew Fryer <andrew.fryer@queensu.ca>
2022-01-22 15:13:15 -0500

    be send correct response

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 
 from parse import parse
-from alg import alg
+# from alg import alg
+from validator import validator, get_delta_distance
+from output import save_file
 
 
 app = Flask(__name__)
@@ -22,8 +24,47 @@ def do_alg():
     csv_file = args.get('csv_file')
 
     df = dfs[csv_file]
-    result = alg(df, a, b)
 
-    return {}
+    # run algorithm
+    # result = alg(df, a, b)
+    solution = df
+
+    solution_file = save_file(solution, csv_file)
+
+    # validate solution
+    QoR = validator(csv_file, solution_file)
+
+    # format for front end
+    route = []
+    prev = None
+    for i, row in solution.iterrows():
+        if prev == None:
+            prev = row
+        else:
+            id = row['id']
+            route.append({
+                "startNodeId": prev['id'],
+                "endNodeId": row['id'],
+                "startLat": prev['latitude'],
+                "startLng": prev['longitude'],
+                "endLat": row['latitude'],
+                "endLng": row['longitude'],
+                "distance": get_delta_distance((prev['latitude'], prev['longitude']), \
+                    (row['latitude'], row['longitude']))
+            })
+            prev = row
+
+    return {
+        "points": df.to_dict('records'),
+        "route": route,
+        "data": {
+            "totalPlasticProduced": 0,
+            "totalPlasticRecycled": 0,
+            "totalPlasticLost": 0,
+            "totalPlasticInOcean": 0,
+            "totalDistance": 0,
+            "QoR": QoR,
+        },
+    }
 
 print('done')
---
+++ src/output.py
@@ -0,0 +1,6 @@
+import os
+
+def save_file(df, file_name):
+    f = os.path.join('../output', file_name)
+    df.to_csv(f)
+    return f
